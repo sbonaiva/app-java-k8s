9f7cc3efe6f4dd61e16caf388c5942de679fec58
Stéffano Bonaiva Batista <steffano@outlook.com>
2023-02-28 13:32:29 -0300

    fix migration

--- src/main/resources/db/migration/V1__create_customer_table.sql
+++ src/main/resources/db/migration/V1__create_customer_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE customer (
 	id BIGSERIAL NOT NULL,
-	customer_id BIGINT,
+	customer_id BIGINT NOT NULL,
 	name VARCHAR(255) NOT NULL,
 	CONSTRAINT pk_customer PRIMARY KEY (id),
 	CONSTRAINT fk_customer_address FOREIGN KEY(customer_id) REFERENCES customer(id)
